7cbcafaa1f7ea85f713c36ac7846232d41f5a23e
Muh-Hasan <muh.hasan.git@gmail.com>
2020-09-15 06:58:37 -0700

    basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import { MultiForm } from './components/multistepForm/index'
 
 function App() {
   return (
-    <div>
-      <h1>hello world</h1>
+    <div className='main-app'>
+      <div className='main-app-2'>
+        <MultiForm />
+      </div>
     </div>
   );
 }
---
+++ src/components/multistepForm/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import * as yup from 'yup'
+import { Formik , Form } from 'formik'
+
+export function MultiForm(){
+    return(
+        <div>
+            <h1>this is form</h1>
+        </div>
+    )
+}
